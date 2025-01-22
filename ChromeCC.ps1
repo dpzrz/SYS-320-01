faa6f9ab4b17b196bebc4e9f34ed2483bb207748
dpzrz <diego.perez@mymail.champlain.edu>
2025-01-22 13:21:24 -0500

    Process Managment -1

---
+++ ChromeCC.ps1
@@ -0,0 +1,10 @@
+﻿$service = Get-Process -Name chrome 
+
+if(-not $service){
+    Start-Process "chrome" "www.champlain.edu"
+    "Chrome is now running"
+
+}else{
+Stop-Process $service
+"chrome is already running"
+}
