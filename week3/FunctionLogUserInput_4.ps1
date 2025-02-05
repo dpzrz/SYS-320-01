f41767b3655d818d5b4f89dbc0f154f62ff9c413
Diego Perez <112894794+dpzrz@users.noreply.github.com>
2025-02-04 22:17:15 -0500

    Update FunctionLogUserInput_4.ps1

--- week3/FunctionLogUserInput_4.ps1
+++ week3/FunctionLogUserInput_4.ps1
@@ -16,7 +16,7 @@
             return $ntAccount.Value
         }
         catch {
-            return $Sid  # Return the original SID if translation fails
+            return $Sid  
         }
     }
 
@@ -24,7 +24,7 @@
     $eventList = @()
 
     Get-EventLog -LogName Security -After $startDate | Where-Object {$_.EventID -in @(4624, 4634)} | ForEach-Object {
-        $sid = $_.ReplacementStrings[4]  # SID is typically in index 4 for these events
+        $sid = $_.ReplacementStrings[4]  
         $username = Convert-SidToUsername -Sid $sid
 
         $event = [PSCustomObject]@{
@@ -39,13 +39,13 @@
     return $eventList
 }
 
-# Prompt the user for the number of days
+
 $userInput = Read-Host "Enter the number of days to retrieve logon/logoff events"
 
-# Validate the input
+
 if ([int]::TryParse($userInput, [ref]$null)) {
     $days = [int]$userInput
-    # Call the function with the user-provided number of days and print the results
+
     $results = Get-LogonLogoffEvents -Days $days
     $results | Format-Table -AutoSize
 }
