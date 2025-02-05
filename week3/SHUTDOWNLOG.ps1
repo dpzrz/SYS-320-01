6f432b40b1bead579cdeb8660c48219eb42fa2ff
Diego Perez <112894794+dpzrz@users.noreply.github.com>
2025-02-04 22:19:26 -0500

    Update SHUTDOWNLOG.ps1

--- week3/SHUTDOWNLOG.ps1
+++ week3/SHUTDOWNLOG.ps1
@@ -7,7 +7,7 @@
     $startDate = (Get-Date).AddDays(-$Days)
     $eventList = @()
 
-    # Get startup events (Event ID 6005)
+
     Get-EventLog -LogName System -After $startDate | Where-Object {$_.EventID -eq 6005} | ForEach-Object {
         $event = [PSCustomObject]@{
             Time  = $_.TimeGenerated
@@ -18,7 +18,7 @@
         $eventList += $event
     }
 
-    # Get shutdown events (Event ID 6006)
+
     Get-EventLog -LogName System -After $startDate | Where-Object {$_.EventID -eq 6006} | ForEach-Object {
         $event = [PSCustomObject]@{
             Time  = $_.TimeGenerated
@@ -32,13 +32,13 @@
     return $eventList
 }
 
-# Prompt the user for the number of days
+
 $userInput = Read-Host "Enter the number of days to retrieve startup/shutdown events"
 
-# Validate the input
+
 if ([int]::TryParse($userInput, [ref]$null)) {
     $days = [int]$userInput
-    # Call the function with the user-provided number of days and print the results
+
     $results = Get-StartupShutdownEvents -Days $days
     $results | Format-Table -AutoSize
 }
