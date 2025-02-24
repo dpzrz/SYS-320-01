6d919bce4ead7046dd7afaeb8f6f4c852254f203
dpzrz <diego.perez@mymail.champlain.edu>
2025-02-24 13:53:53 -0500

    Scraping

---
+++ week5/Callfile.ps1
@@ -0,0 +1,33 @@
+﻿. "C:\Users\champuser\SYS-320-01\week5\gatherClasses.ps1"
+
+$classes = Translate-DaysToArray(gatherClasses)
+
+#Write-Output $classes
+
+
+$furkanClasses = $classes | Where-Object {$_.Instructor -eq "Furkan Paligu"}
+
+Write-Output $furkanClasses
+$joyc310Mondays = $classes | Where-Object {
+   # $_. "Class Code" -eq "JOYC 310" -and $_.Days -contains "Monday"
+}
+
+ #Sort by Start Time
+ $joyc310MondaysSorted = $joyc310Mondays | Sort-Object "Time Start"
+
+classes | Where-Object { ($_.Location -eq "JOYC 310") -and ($_.Days -contains "Monday") } | 
+Sort-Object "Time Start" | Format-Table "Time Start", "Time End", "Class Code"
+
+
+<#$classes | Where-Object {
+    $_. "Class Code" -like "SYS*" -or
+    $_. "Class Code" -like "NET*" -or
+    $_. "Class Code" -like "SEC*" -or
+    $_. "Class Code" -like "FOR*" -or
+    $_. "Class Code" -like "CSI*" -or
+    $_. "Class Code" -like "DAT*"
+`} | Select-Object Instructor -Unique | Sort-Object Instructor
+#>
+
+
+$classes | Group-Object Instructor | Select-Object Name, Count | Sort-Object Count -Descending
---
+++ week5/gatherClasses.ps1
@@ -0,0 +1,40 @@
+﻿function gatherClasses() {
+    $page = Invoke-WebRequest -TimeoutSec 2 -Uri "http://10.0.17.16/Courses.html"  # Replace with actual URL
+    $trs = $page.ParsedHtml.getElementsByTagName("tr")
+    $FullTable = @()
+
+    for ($i = 1; $i -lt $trs.length; $i++) {
+        $tds = $trs[$i].getElementsByTagName("td")
+        $Times = $tds[5].innerText.Split(" - ") #Splitting time
+
+        $FullTable += [PSCustomObject]@{
+            "Class Code"  = $tds[0].innerText
+            "Title"       = $tds[1].innerText
+            "Days"        = $tds[4].innerText
+            "Time Start"  = $Times[0]
+            "Time End"    = $Times[1]
+            "Instructor"  = $tds[6].innerText
+            "Location"    = $tds[9].innerText
+        }
+  }
+ 
+ return $FullTable 
+}
+
+function Translate-DaysToArray($FullTable) {
+
+    foreach ($record in $FullTable) {
+        $Days = @() # Empty array to hold days for every record
+
+        if ($record.Days -ilike "*M*") { $Days += "Monday" }
+        if (($record.Days -ilike "*T*") -and ($record.Days -notlike "*TH*")) { $Days += "Tuesday" }
+        if ($record.Days -ilike "*W*") { $Days += "Wednesday" }
+        if ($record.Days -ilike "*TH*") { $Days += "Thursday" }
+        if ($record.Days -ilike "*F*") { $Days += "Friday" }
+
+        $record.Days = $Days
+    }
+
+    return $FullTable
+}
+
